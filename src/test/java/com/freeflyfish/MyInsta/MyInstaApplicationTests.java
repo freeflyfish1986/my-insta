c99873d417b5dfdd7dd0eb819499cbb15762484c
Freeflyfish <freeflyfish.login@mail.ru>
2025-10-30 11:39:39 +0300

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/freeflyfish/MyInsta/MyInstaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freeflyfish.MyInsta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyInstaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
